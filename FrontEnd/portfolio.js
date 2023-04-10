63c57fedb81d02e2cded9b60010275c9a4847974
KenfackMF <francis.kenfack@gmail.com>
2023-04-11 00:02:45 +0200

    ajout du login

--- FrontEnd/portfolio.js
+++ FrontEnd/portfolio.js
@@ -1,4 +1,6 @@
+
 let catalogue = [];
+let categories = [];
 
 const container = document.querySelector(".gallery");
 
@@ -7,6 +9,7 @@ fetch("http://localhost:5678/api/works")
   .then((data) => {
     catalogue = data;
     genererCatalogue(catalogue);
+    console.log(catalogue)
   });
 
 function genererCatalogue(catalogue) {
@@ -24,55 +27,50 @@ function genererCatalogue(catalogue) {
   }
 }
 
-// let categories = [];
-
-//  fetch('http://localhost:5678/api/categories')
-//  .then(reponse => reponse.json())
-//  .then(data =>{
-//   categories = data
-//   console.log(categories);
-//  })
-
-// Récupérer le tableau depuis l'API
 fetch("http://localhost:5678/api/categories")
-  .then((response) => response.json())
+  .then((reponse) => reponse.json())
   .then((data) => {
-    // Tri par ID
-    data.sort((a, b) => a.id - b.id);
-    // Créer une liste des catégories
-    let categories = ["Tous", "Objet", "Appartements", "Hôtels & restaurant"];
-    // Créer une section pour afficher les catégories
-    let sectionCategories = document.createElement("section");
-    // Ajouter un titre à la section
-    let titreSection = document.createElement("h2");
-    titreSection.textContent = "Catégories";
-    sectionCategories.appendChild(titreSection);
-    // Créer un élément de liste pour chaque catégorie et les ajouter à la section
-    let listeCategories = document.createElement("ul");
-    for (let i = 0; i < categories.length; i++) {
-      let categorie = categories[i];
-      let listeElement = document.createElement("li");
-      listeElement.textContent = categorie;
-      // Ajouter un événement de clic pour filtrer les éléments en fonction de la catégorie
-      listeElement.addEventListener("click", () => {
-        filtrerParCategorie(categorie, data);
-      });
-      listeCategories.appendChild(listeElement);
-    }
-    sectionCategories.appendChild(listeCategories);
-    // Ajouter la section au document
-    document.body.appendChild(sectionCategories);
-  })
-  .catch((error) => console.error(error));
+    categories = data
+    console.log(categories);
+    const categoriesContainer = document.getElementById('categories');
+    const photosContainer = document.getElementsByClassName('gallery');
 
-// Fonction pour filtrer les éléments en fonction de la catégorie
-function filtrerParCategorie(categorie, data) {
-  let elementsFiltres = [];
-  if (categorie === "tous") {
-    elementsFiltres = data;
-  } else {
-    elementsFiltres = data.filter((element) => element.categorie === categorie);
-  }
-  // Afficher les éléments filtrés dans la console
-  console.log(elementsFiltres);
-}
+    const categoryMap = new Map();
+    categories.forEach(category => categoryMap.set(category.id, category));
+
+
+    function updatePhotos(categoryId) {
+      while (photosContainer[0].firstChild) {
+        photosContainer[0].removeChild(photosContainer[0].firstChild);
+      }
+      const filteredPhotos = catalogue.filter(photo => categoryId === 0 || photo.categoryId === categoryId);
+      filteredPhotos.forEach(photo => {
+        const figurePhoto = document.createElement("figure");
+        const imageBal = document.createElement("img");
+
+        imageBal.setAttribute("src", photo.imageUrl);
+        const titrePhoto = document.createElement("p");
+        titrePhoto.innerText = photo.title;
+
+        photosContainer[0].appendChild(figurePhoto);
+        figurePhoto.appendChild(imageBal);
+        figurePhoto.appendChild(titrePhoto);
+      });
+    }
+
+    categoriesContainer.addEventListener('click', event => {
+      if (event.target.tagName === 'BUTTON') {
+        const categoryId = parseInt(event.target.dataset.categoryId);
+        updatePhotos(categoryId);
+      }
+    });
+
+    updatePhotos(0);
+  });
+
+
+  const button = document.querySelector('button');
+
+  button.addEventListener('click', () => {
+    button.style.backgroundColor = '#0E2F28';
+  });
