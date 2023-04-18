54a780210129f03f5c753ef3e1372e3660685868
KenfackMF <francis.kenfack@gmail.com>
2023-04-18 18:18:08 +0200

    ajout du login html et js

---
+++ FrontEnd/login.js
@@ -0,0 +1,47 @@
+const form = document.querySelector('form');
+const erreurSaisi = document.createElement('p');
+erreurSaisi.textContent = 'Adresse mail ou mot de passe invalide';
+erreurSaisi.style.color = 'red';
+erreurSaisi.style.display = 'none';
+erreurSaisi.setAttribute('id', 'erreurSaisi');
+form.appendChild(erreurSaisi);
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const donnEntre = {
+    email: "",
+    password: "",
+  };
+
+  const adresseMail = document.querySelector('#adresseMail');
+  const motDePasse = document.querySelector('#motDePasse');
+
+  adresseMail.addEventListener("input", (e) => {
+    donnEntre.email = e.target.value;
+  });
+
+  motDePasse.addEventListener("input", (e) => {
+    donnEntre.password = e.target.value;
+  });
+
+  fetch('http://localhost:5678/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(donnEntre)
+  })
+  .then((reponse) => reponse.json())
+  .then((data) => {
+    donneData = data
+    console.log(donneData);
+  
+    localStorage.setItem('token', data.token);
+    window.location.href = 'index.html';
+  })
+  .catch((error) => {
+    console.error(error);
+    document.querySelector('#erreurSaisi').style.display = 'block';
+  });
+});
--- FrontEnd/portfolio.js
+++ FrontEnd/portfolio.js
@@ -1,4 +1,3 @@
-
 let catalogue = [];
 let categories = [];
 
@@ -38,39 +37,76 @@ fetch("http://localhost:5678/api/categories")
     const categoryMap = new Map();
     categories.forEach(category => categoryMap.set(category.id, category));
 
+    // Ajouter la catégorie "Tous"
+    const tousButton = document.createElement('button');
+    tousButton.textContent = 'Tous'; 
+    tousButton.dataset.categoryId = 0;
+    tousButton.classList.add('category-button');
+    categoriesContainer.appendChild(tousButton);
+    
+    // Ajouter l'écouteur d'événements pour le bouton 'Tous'
+    tousButton.addEventListener('click', () => {
+      updatePhotos(0);
+      // Changer la couleur de fond et le texte du bouton 'Tous'
+      tousButton.style.backgroundColor = '#0E2F28';
+      tousButton.style.color = 'white';
+      // Réinitialiser la couleur de fond et le texte de tous les autres boutons de catégorie
+      const buttons = document.querySelectorAll('.category-button');
+      buttons.forEach(button => {
+        if (button !== tousButton) {
+          button.style.backgroundColor = '';
+          button.style.color = '';
+        }
+      });
+    });
+
+    
+    
+    categories.forEach(category => {
+      const button = document.createElement('button');
+      button.textContent = category.name; 
+      button.dataset.categoryId = category.id;
+      button.classList.add('category-button');
+      categoriesContainer.appendChild(button);
+      // Ajouter l'écouteur d'événements pour chaque bouton de catégorie
+      button.addEventListener('click', () => {
+        const categoryId = parseInt(button.dataset.categoryId);
+        updatePhotos(categoryId);
+        // Changer la couleur de fond et le texte du bouton actif
+        button.style.backgroundColor = '#0E2F28';
+        button.style.color = 'white';
+        // Réinitialiser la couleur de fond et le texte de tous les autres boutons de catégorie
+        const buttons = document.querySelectorAll('.category-button');
+        buttons.forEach(btn => {
+          if (btn !== button && btn !== tousButton) {
+            btn.style.backgroundColor = '';
+            btn.style.color = '';
+          }
+        });
+      });
+    });
+
+
 
     function updatePhotos(categoryId) {
       while (photosContainer[0].firstChild) {
         photosContainer[0].removeChild(photosContainer[0].firstChild);
       }
-      const filteredPhotos = catalogue.filter(photo => categoryId === 0 || photo.categoryId === categoryId);
+
+      // Modifier la condition pour afficher toutes les photos
+      const filteredPhotos = categoryId === 0 ? catalogue : catalogue.filter(photo => photo.categoryId === categoryId);
       filteredPhotos.forEach(photo => {
         const figurePhoto = document.createElement("figure");
         const imageBal = document.createElement("img");
 
         imageBal.setAttribute("src", photo.imageUrl);
+
         const titrePhoto = document.createElement("p");
         titrePhoto.innerText = photo.title;
-
-        photosContainer[0].appendChild(figurePhoto);
+    
+        container.appendChild(figurePhoto);
         figurePhoto.appendChild(imageBal);
         figurePhoto.appendChild(titrePhoto);
       });
     }
-
-    categoriesContainer.addEventListener('click', event => {
-      if (event.target.tagName === 'BUTTON') {
-        const categoryId = parseInt(event.target.dataset.categoryId);
-        updatePhotos(categoryId);
-      }
-    });
-
-    updatePhotos(0);
-  });
-
-
-  const button = document.querySelector('button');
-
-  button.addEventListener('click', () => {
-    button.style.backgroundColor = '#0E2F28';
-  });
+});
